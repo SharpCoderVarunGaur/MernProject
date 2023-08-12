a01ba77331e5e91db79a8c631b66088459f3545d
SharpCoderVarunGaur <varungaur408@gmail.com>
2023-08-12 12:17:59 +0530

    ADD::Css

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route ,Routes} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About'
+import Contact from './Components/contact';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+
+
+  <Navbar/>
+  
+  <Routes>
+
+  <Route path='/'  element={
+    <Home/>
+  } />
+ 
+  <Route path='/about'  element={<About />}  />
+  
+
+  <Route path='/contact' element={<Contact />} />
+
+  <Route path='/login' element={<Login />} />
+
+
+  <Route path='/signup' element={<Signup />} />
+ 
+  </Routes>
+ 
+ </>
   );
 }
 
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,81 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.css'
+import { NavLink } from 'react-router-dom'
+import logo from '../Images/IMG_20200702_203712_884.jpg'
+const Navbar = () => {
+  return (
+   <>
+
+    {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <NavLink className="navbar-brand" to="#">
+
+        <img height="50px" src={logo} alt=""
+        />
+    </NavLink>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <NavLink className="nav-link" to="/">Home </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/about">About</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/contact">Contact</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/login">Login</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/signup">Registration</NavLink>
+      </li>
+    </ul>
+    
+  </div>
+</nav> */}
+<nav className='navbar backround h-nav'>
+<ul className="nav-list v">
+  <div className='logo'><img  src={logo} alt=''/> </div>
+        <li >
+        <NavLink className="nav-link" to="/">Home </NavLink>
+        </li>
+       <li >
+        <NavLink className="nav-link"   to="/about">About</NavLink>
+       </li>
+      <li >
+        <NavLink className="nav-link" to="/contact">Contact</NavLink>
+       </li>
+      <li >
+        <NavLink className="nav-link" to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink className="nav-link" to="/signup">Registration</NavLink>
+      </li>
+</ul>
+<div className='rightNav v'>
+<input
+        type="text"
+        id="search"
+        name="search"
+        // value={inputValue}
+        // onChange={handleInputChange}
+      />
+     <button className='btn btn-sm'>Click Me</button>
+</div>
+<div className='burger'>
+  <div className='line'></div>
+  <div className='line'></div>
+  <div className='line'></div>
+</div>
+
+</nav>
+
+   </>
+  )
+}
+
+export default Navbar
